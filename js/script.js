0327b75d604041784e9b3c949f5717771593f4e9
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-03-02 18:49:21 +0100

    -sto provando a non far sommare le griglie quando seleziono un'altra difficoltà-

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// L'utente indica un livello di difficoltà in base al quale viene 
+// generata una griglia di gioco quadrata, 
+// in cui ogni cella contiene un numero (in ordine) 
+// tra quelli compresi in un range:
+// con difficoltà 1 => tra 1 e 100
+// con difficoltà 2 => tra 1 e 81
+// con difficoltà 3 => tra 1 e 49
+
+let btnPlay = document.querySelector('#btn-play');
+
+btnPlay.addEventListener('click', function() {
+    let easy = document.querySelector('#easy');
+    let medium = document.querySelector('#medium');
+    let hard = document.querySelector('#hard');
+    let output = document.querySelector('.output');
+    let outputTwo = document.querySelector('.output-two');
+    output.classList.add('shadow');
+    
+
+    if(easy.selected) {
+        for (let i = 1; i <= 100; i++) {
+            let elemento = document.createElement('div');
+            elemento.classList.add('box');
+            elemento.innerHTML = i;
+            outputTwo.append(elemento);
+            
+        } 
+    } else if (medium.selected) {
+        for (let i = 1; i <= 81; i++) {
+            let elemento = document.createElement('div');
+            elemento.classList.add('box');
+            elemento.innerHTML = i;
+            outputTwo.append(elemento);
+           
+        }
+
+    } else if (hard.selected) {
+        for (let i = 1; i <= 49; i++) {
+            let elemento = document.createElement('div');
+            elemento.classList.add('box');
+            elemento.innerHTML = i;
+            outputTwo.append(elemento);
+            
+        }
+
+    }
+    
+})
+   
+
+
+
